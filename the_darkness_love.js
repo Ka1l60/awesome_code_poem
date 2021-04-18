5bedea81bbb442c930212a0ccfde649cabd125d9
Ka1l60 <80076696+Ka1l60@users.noreply.github.com>
2021-04-18 19:41:06 +0200

    Create the_darkness_love.js

---
+++ the_darkness_love.js
@@ -0,0 +1,14 @@
+(promises) => {
+  if (i.make === you) {
+    a = new Promise((t, hat) => {
+      t(ogether, love, shall).bind(us);
+    })
+      .then((will) => {
+        you();
+      })
+      .catch((me) => {
+        as = we.fall("?");
+      });
+    return { my: Promise.reject(the.darkness) };
+  }
+};
